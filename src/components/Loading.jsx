3e1c2763db9a1cab4f55a8e3c4fc03d0944d9795
HesamMirzaie <mirzaeeh08@gmail.com>
2024-06-13 12:32:50 +0330

    version 1

---
+++ src/components/Loading.jsx
@@ -0,0 +1,13 @@
+import { ImSpinner8 } from 'react-icons/im';
+
+const Loading = () => {
+  return (
+    <div className=" w-full h-screen bg-gradientBg bg-no-repeat bg-cover bg-center flex flex-col justify-center items-center">
+      <div>
+        <ImSpinner8 className=" text-5xl animate-spin text-white" />
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
