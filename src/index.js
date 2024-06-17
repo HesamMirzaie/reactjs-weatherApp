b3f3d02dcf4334e14a85af256bf6b919bae76d0d
HesamMirzaie <mirzaeeh08@gmail.com>
2024-06-17 21:39:48 +0330

    remove context

--- src/index.js
+++ src/index.js
@@ -2,11 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { WeatherContext } from './context/context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <WeatherContext.Provider value={user}>
-    <App />
-  </WeatherContext.Provider>
-);
+root.render(<App />);
